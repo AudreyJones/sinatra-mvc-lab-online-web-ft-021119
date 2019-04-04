99daa9c3f066535ad601da40fab36cc6a540574e
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-04 15:01:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+
+  end
+
+  post '/piglatinize' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    erb :user_input
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,41 @@
+class PigLatinizer
+#Convert a string into pig latin!
+  def initialize
+  end
+
+  def word_piglatinize (str)
+    str1="aeiou"
+    str2=(/\A[aeiou]/) # is the first letter a vowel?
+    vowel = str1.scan(/\w/)
+    alpha =('a'..'z').to_a
+    con = (alpha - vowel).join
+    word = str.downcase.scan(/\w/)
+    if vowel.include?(word[0]) #word begins with a vowel sound, add an "ay" sound to the end of the word
+      str =~ str2
+      str + "way"
+    elsif (con.include?(word[0])) && (con.include?(word[1])) && (con.include?(word[2]))
+      # binding.pry
+      # word[0..2]=~(/\A[con]/)
+      s = str.slice!(/^.../)
+      str + s + "ay"
+    elsif (con.include?(word[0])) && (con.include?(word[1]))
+      #word[0..1]=~(/\A[con]/)
+      s = str.slice!(/^../)
+      str + s + "ay"
+    elsif con.include?(word[0])# word begins with a consonant sound, move it to the end of the word, and then add an "ay" sound to the end of the word
+      str != str2
+      s = str.slice!(/^./)
+      str + s + "ay"
+    end
+  end
+
+  def piglatinize(sentence)
+    #do something to split sentence into array
+    # if array > 1
+    #loop
+    # else
+    #do word_piglatinize
+
+  end
+
+end
